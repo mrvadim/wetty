8411d7ba347316a4f4a101883a33addb418f5daa
Alexander Myznikov <post@edev-service.ru>
2018-02-21 09:59:32 +0300

    Make Wetty exportable.
    Add start and stop exportable functions to app.js.
    Bring ES6 syntax to app.js entirely.
    Remove spawning terminal with /usr/bin/env command in case of running Wetty server as root.
    Change Express module dependency version to 4.16.2.

--- app.js
+++ app.js
@@ -1,109 +1,80 @@
-var express = require('express');
-var http = require('http');
-var https = require('https');
-var path = require('path');
-var server = require('socket.io');
-var pty = require('pty.js');
-var fs = require('fs');
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const server = require('socket.io');
+const pty = require('pty.js');
+const fs = require('fs');
 
-var opts = require('optimist')
-    .options({
-        sslkey: {
-            demand: false,
-            description: 'path to SSL key'
-        },
-        sslcert: {
-            demand: false,
-            description: 'path to SSL certificate'
-        },
-        sshhost: {
-            demand: false,
-            description: 'ssh server host'
-        },
-        sshport: {
-            demand: false,
-            description: 'ssh server port'
-        },
-        sshuser: {
-            demand: false,
-            description: 'ssh user'
-        },
-        sshauth: {
-            demand: false,
-            description: 'defaults to "password", you can use "publickey,password" instead'
-        },
-        port: {
-            demand: true,
-            alias: 'p',
-            description: 'wetty listen port'
-        },
-    }).boolean('allow_discovery').argv;
+let httpserv;
 
-var runhttps = false;
-var globalsshport = 22;
-var globalsshhost = 'localhost';
-var globalsshauth = 'password,keyboard-interactive';
-var globalsshuser = '';
+exports.start = function(opts) {
+  // Defaults for ssh terminal to be run with
+  let runhttps = false;
+  let globalsshport = 22;
+  let globalsshhost = 'localhost';
+  let globalsshauth = 'password,keyboard-interactive';
+  let globalsshuser = '';
 
-if (opts.sshport) {
+  // Change defaults if passed
+  if (opts.sshport) {
     globalsshport = opts.sshport;
-}
+  }
 
-if (opts.sshhost) {
+  if (opts.sshhost) {
     globalsshhost = opts.sshhost;
-}
+  }
 
-if (opts.sshauth) {
-	globalsshauth = opts.sshauth;
-}
+  if (opts.sshauth) {
+    globalsshauth = opts.sshauth;
+  }
 
-if (opts.sshuser) {
+  if (opts.sshuser) {
     globalsshuser = opts.sshuser;
-}
+  }
 
-if (opts.sslkey && opts.sslcert) {
+  if (opts.sslkey && opts.sslcert) {
     runhttps = true;
     opts['ssl'] = {};
     opts.ssl['key'] = fs.readFileSync(path.resolve(opts.sslkey));
     opts.ssl['cert'] = fs.readFileSync(path.resolve(opts.sslcert));
-}
+  }
 
-process.on('uncaughtException', function(e) {
-    console.error('Error: ' + e);
-});
+  process.on('uncaughtException', function(e) {
+    console.error('Error in Wetty terminal: ' + e);
+  });
 
-var httpserv;
+  const app = express();
 
-var app = express();
+  app.get('/wetty/ssh', (req, res) => {
+    res.sendFile(__dirname + '/public/wetty/index.html');
+  });
+  app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/wetty/ssh', function(req, res) {
-    res.sendfile(__dirname + '/public/wetty/index.html');
-});
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-if (runhttps) {
+  if (runhttps) {
     httpserv = https.createServer(opts.ssl, app).listen(opts.port, function() {
-        console.log('https on port ' + opts.port);
+      console.log('https on port ' + opts.port);
     });
-} else {
+  } else {
     httpserv = http.createServer(app).listen(opts.port, function() {
-        console.log('http on port ' + opts.port);
+      console.log('http on port ' + opts.port);
     });
-}
+  }
 
-var io = server(httpserv,{path: '/wetty/socket.io'});
-io.on('connection', function(socket){
+  const io = server(httpserv,{path: '/wetty/socket.io'});
+
+  io.on('connection', function(socket){
     const request = socket.request;
     let credentials = {};
 
     console.log((new Date()) + ' Connection accepted.');
 
     if (match = request.headers.referer.match(/\/wetty\/ssh\?(.*)#?/)) {
-        credentials = match[1].split('&').reduce((acc, cur) => {
-            const entry = cur.split('=');
+      credentials = match[1].split('&').reduce((acc, cur) => {
+        const entry = cur.split('=');
 
-            return Object.assign(acc, { [entry[0].toLowerCase()]: entry[1] });
-        }, {});
+        return Object.assign(acc, { [entry[0].toLowerCase()]: entry[1] });
+      }, {});
     }
 
     let sshuser = credentials['sshuser'] || globalsshuser;
@@ -112,45 +83,44 @@ io.on('connection', function(socket){
     const sshauth = globalsshauth;
 
     if (sshuser) {
-        sshuser += '@';
+      sshuser += '@';
     }
 
     if (process.env.DEBUG) {
-        console.log(
-            (new Date()) + " REFERER", request.headers.referer,
-            "PARSED INTO CREDENTIALS", sshuser, sshhost, sshport
-        );
+      console.log(
+        (new Date()) + " REFERER", request.headers.referer,
+        "PARSED INTO CREDENTIALS", sshuser, sshhost, sshport
+      );
     }
 
-    var term;
-    if (process.getuid() == 0) {
-        term = pty.spawn('/usr/bin/env', ['login'], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    } else {
-        term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    }
+    const term = pty.spawn('ssh', [
+      sshuser + sshhost,
+      '-p',
+      sshport,
+      '-o',
+      'PreferredAuthentications=' + sshauth
+    ], { name: 'xterm-256color', cols: 80, rows: 30 });
+
     console.log((new Date()) + " PID=" + term.pid + " STARTED on behalf of user=" + sshuser);
     term.on('data', function(data) {
-        socket.emit('output', data);
+      socket.emit('output', data);
     });
     term.on('exit', function(code) {
-        console.log((new Date()) + " PID=" + term.pid + " ENDED");
+      console.log((new Date()) + " PID=" + term.pid + " ENDED");
     });
     socket.on('resize', function(data) {
-        term.resize(data.col, data.row);
+      term.resize(data.col, data.row);
     });
     socket.on('input', function(data) {
-        term.write(data);
+      term.write(data);
     });
     socket.on('disconnect', function() {
-        console.log((new Date()) + " PID=" + term.pid + " DISCONNECTED");
-        term.end();
+      console.log((new Date()) + " PID=" + term.pid + " DISCONNECTED");
+      term.end();
     });
-})
+  });
+}
+
+exports.stop = function() {
+  httpserv.close();
+}
